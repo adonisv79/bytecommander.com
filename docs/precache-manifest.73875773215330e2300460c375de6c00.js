6a3582b0f796c05d048624c8937bae54f60dc0fb
Adonis Lee Villamor <adonisv79@gmail.com>
2020-02-23 20:59:32 +0800

    Testing build output as 'docs' for my github pages

---
+++ docs/precache-manifest.73875773215330e2300460c375de6c00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12068c860f077c2a5a124e6250a9fd7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e95cf8cad7c752209fc2",
+    "url": "/static/css/main.9f2d9a23.chunk.css"
+  },
+  {
+    "revision": "e5591961bfdb6aa3a4ea",
+    "url": "/static/js/2.4fa42b6d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4fa42b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95cf8cad7c752209fc2",
+    "url": "/static/js/main.f72989ff.chunk.js"
+  },
+  {
+    "revision": "430402cf6d7307fdc483",
+    "url": "/static/js/runtime-main.073befff.js"
+  }
+]);
